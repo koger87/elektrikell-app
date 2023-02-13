3ba8f0a39117d5f723b626055ed82a70b18f953d
marija koger <marija.koger@gmail.com>
2023-02-13 18:35:12 +0200

    homework

---
+++ src/FooterHighPrice.js
@@ -0,0 +1,31 @@
+import Container from 'react-bootstrap/Container';
+
+
+function FooterHighPrice() {
+return (
+<Container className="text-center">
+<div>FooterHighPrice</div>
+
+</Container>
+);
+}
+
+export default FooterHighPrice;
+
+
+
+
+// import HourContainer from 
+
+
+// function PeakHourComponent() {
+// return (
+// <HourContainer>
+// <div style="padding: 4px 12px; cursor: pointer; border-radius: 5px; background-color: rgb(255, 255, 255);">
+// <span style="font-weight: 600; color: rgb(54, 74, 99);">Tiputunnid</span>
+// </div>
+// </HourContainer>
+// )
+// }
+
+// export default PeakHourComponent
